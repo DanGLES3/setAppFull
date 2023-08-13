4ef0bc0b401a9593c2863e6aff07acaceb6514b8
DanGLES3 <switchtegra@gmail.com>
2023-08-13 03:53:38 -0300

    Update MainHook.java

--- app/src/main/java/ss/colytitse/setappfull/MainHook.java
+++ app/src/main/java/ss/colytitse/setappfull/MainHook.java
@@ -69,7 +69,7 @@ public class MainHook implements IXposedHookLoadPackage {
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 super.afterHookedMethod(param);
                 Activity activity = (Activity) param.thisObject;
-                activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 );
                 WindowManager.LayoutParams lp = activity.getWindow().getAttributes();
