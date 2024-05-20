31ee26ab2a4d62c92f06b51512315148fa51c5d7
DanGLES3 <switchtegra@gmail.com>
2024-05-20 03:08:08 -0300

    Update MainHook.java

--- app/src/main/java/ss/colytitse/setappfull/MainHook.java
+++ app/src/main/java/ss/colytitse/setappfull/MainHook.java
@@ -17,15 +17,26 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class MainHook implements IXposedHookLoadPackage {
 
     private static final String TAG = "test_";
-    private static final String SystemMode;
-    private static final String TimelyMode;
-    private static final boolean ScopeMode;
+    private static XSharedPreferences xsp;
+
     static {
-        XSharedPreferences xsp = new XSharedPreferences(BuildConfig.APPLICATION_ID,"config");
+        xsp = new XSharedPreferences(BuildConfig.APPLICATION_ID, "config");
         xsp.makeWorldReadable();
-        SystemMode = xsp.getString("SystemMode", "");
-        TimelyMode = xsp.getString("TimelyMode", "");
-        ScopeMode = xsp.getBoolean("scope_mode_switch", true);
+    }
+
+    private String getSystemMode() {
+        xsp.reload(); // Reload to get the latest values
+        return xsp.getString("SystemMode", "");
+    }
+
+    private String getTimelyMode() {
+        xsp.reload(); // Reload to get the latest values
+        return xsp.getString("TimelyMode", "");
+    }
+
+    private boolean isScopeModeEnabled() {
+        xsp.reload(); // Reload to get the latest values
+        return xsp.getBoolean("scope_mode_switch", true);
     }
 
     @Override
@@ -34,7 +45,7 @@ public class MainHook implements IXposedHookLoadPackage {
             return;
         if (!lpparam.packageName.equals(lpparam.processName))
             return;
-        if ((ScopeMode || TimelyMode.contains(lpparam.packageName)) && !lpparam.packageName.equals("android")){
+        if ((isScopeModeEnabled() || getTimelyMode().contains(lpparam.packageName)) && !lpparam.packageName.equals("android")) {
             onTimelyMode();
             return;
         }
@@ -50,17 +61,17 @@ public class MainHook implements IXposedHookLoadPackage {
                 WindowManager.LayoutParams attrs = (WindowManager.LayoutParams) getObjectField(param.args[0], "mAttrs");
                 if (attrs.type > WindowManager.LayoutParams.LAST_APPLICATION_WINDOW)
                     return;
-                if (SystemMode.contains(attrs.packageName))
+                if (getSystemMode().contains(attrs.packageName))
                     attrs.layoutInDisplayCutoutMode = LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
             }
         };
-        try{
+        try {
             findAndHookMethod("com.android.server.wm.DisplayPolicy", lpparam.classLoader,
-                    "layoutWindowLw","com.android.server.wm.WindowState",
+                    "layoutWindowLw", "com.android.server.wm.WindowState",
                     "com.android.server.wm.WindowState", "com.android.server.wm.DisplayFrames",
                     MethodHook
             );
-        }catch (Throwable ignored){}
+        } catch (Throwable ignored) {}
     }
 
     private void onTimelyMode() {
@@ -69,8 +80,12 @@ public class MainHook implements IXposedHookLoadPackage {
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 super.afterHookedMethod(param);
                 Activity activity = (Activity) param.thisObject;
-                activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 );
                 WindowManager.LayoutParams lp = activity.getWindow().getAttributes();
                 lp.layoutInDisplayCutoutMode = LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
@@ -78,5 +93,4 @@ public class MainHook implements IXposedHookLoadPackage {
             }
         });
     }
-
 }
